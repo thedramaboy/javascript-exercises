1bd1d84aee76889f99182dd55337208bd7312bef
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-01-03 16:43:56 -0700

    Exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let result = '';
+
+    if (num < 0) {
+        return 'ERROR';
+    
+    } else if (num == 0) {
+        return result;
+    } else  {
+        for (let i = 0; i < num; i++) {
+            result += string;
+        }
+    }
+
+    return result;
 
 };
 
