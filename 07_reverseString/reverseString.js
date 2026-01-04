45d3401ca8c6b5c2d472cad36dc30a18a2eb7082
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-01-03 21:32:25 -0700

    Exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const reverseArr = [];
 
+    for (let i = string.length; i >= 0; i--) {
+        reverseArr.push(string[i]);
+    }
+    return reverseArr.join("");
 };
 
 // Do not edit below this line
