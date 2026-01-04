136d69160bfb2c6a27f4f583bf999650ca9d2ab0
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-01-04 15:39:03 -0700

    Exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,31 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+
+    let sum = 0;
+
+    if (typeof firstNum !== "number" || typeof lastNum !== "number") {
+        return 'ERROR';
+    } else if (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+        return 'ERROR';
+    } else if (firstNum <= 0 || lastNum <= 0) {
+        return 'ERROR';
+    }
+
+    if (firstNum < lastNum) {
+
+        for (let i = firstNum; i <= lastNum; i++) {
+            sum = sum + i;
+        }
+
+        return sum;
+
+    } else if (firstNum > lastNum) {
+
+        for (let i = firstNum; i >= lastNum; i--) {
+            sum = sum + i;
+        }
+
+        return sum;
+    }
 
 };
 
