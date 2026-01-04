37c06daa01c82048637f312757dabafa47c9c604
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-01-03 22:18:54 -0700

    Exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removes) {
+    const newArr = [];
+
+    for (const element of arr) {
+        if (!removes.includes(element)) {
+            newArr.push(element);
+        }
+    }
+
+    return newArr;
+
 };
 
 // Do not edit below this line
