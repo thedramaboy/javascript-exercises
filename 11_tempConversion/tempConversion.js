d089b07c2fc90d92416cd08e7391fef7ae8f74bf
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-01-04 21:22:30 -0700

    Exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let celsius = (number - 32) * (5/ 9);
+  return Math.round(celsius * 10)/ 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let fahrenheit = (number * (9/ 5) + 32);
+  return Math.round(fahrenheit * 10)/ 10;
 };
 
 // Do not edit below this line
